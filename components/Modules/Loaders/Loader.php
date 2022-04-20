ed5b0962249c13706cd11557908dac66d7b05fa2
edmariooliver <edmariooliver@outlook.com>
2022-04-19 23:49:07 -0300

    Criando base de Loaders

---
+++ components/Modules/Loaders/Loader.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace SM\Modules\Loaders;
+
+class Loader 
+{
+    /**
+     * File Path
+     */
+    protected String $filePath;
+
+    /**
+     * Layer name to load
+     */
+    protected String $layerName = '';
+
+
+    /**
+     * Set file path  
+     * 
+     * @param String $filePath
+     * @return \Exception
+     * @return String 
+     */
+    protected function _setFilePath(String $filePath)
+    {
+        $pathLayer = explode("components", __DIR__)[0]."app\\".$this->layerName;
+
+        if($this->layerName == '' || !$this->_checkLayer($pathLayer))
+        {
+            throw new \Exception("Layer path undefined");
+        }
+
+        $this->filePath = $pathLayer."\\".$filePath.".php";
+        return $filePath;
+    }
+
+    /**
+     * Method that dynamically loads helpers
+     *
+     *  @param String $fileLoad
+     *  @return \Exception
+     *  @return Bool 
+     */
+    public function load(String $fileLoad)
+    {
+        $this->_setFilePath($fileLoad);
+
+        if($this->_checkFile($this->filePath))
+        {
+            require_once($this->filePath);
+            return true;
+        }else{
+            return throw new \Exception(" not found");
+        }
+    }
+
+    /**
+     * If the layer exists, the method returns True, otherwise False
+     * 
+     * @param String $layerPath
+     * @return Bool
+     */
+    private function _checkLayer(String $layerPath)
+    {
+        return (file_exists($layerPath)) ? true : false;
+    }
+
+    /**
+     * If the file exists, the method returns True, otherwise False
+     * 
+     * @param String $fileName
+     * @return Bool
+     */
+    private function _checkFile(String $fileName)
+    {
+        return (file_exists($fileName)) ? true : false;
+    }
+} 
